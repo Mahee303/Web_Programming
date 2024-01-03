b62d3d6b954ba8e0c825ca704a10706e2ac56893
Mahee303 <155056179+Mahee303@users.noreply.github.com>
2024-01-03 22:45:06 +0530

    Add files via upload

--- catch.php
+++ catch.php
@@ -1,25 +1,34 @@
 <?php
-$fname=$_GET['f_name'];
-$lname=$_GET['l_name'];
-$class=$_GET['class'];
-$div=$_GET['div'];
-$city=$_GET['city'];
-$address=$_GET['adrs'];
-$mobileno=$_GET['mno'];
-$pincode=$_GET['pcode'];
-$state=$_GET['state'];
-$country=$_GET['country'];
-$submit=$_GET['sub'];
-echo"<table border=1 bgcolor#F08080	 align=center>
-<tr> <center>REGISTRATION FORM </center> </tr>
-<tr> <td>FIRST NAME:</td> <td>$fname</td> </tr> 
-<tr> <td>LAST NAME:</td> <td>$lname</td> </tr>
-<tr> <td>CLASS:</td> <td>$class</td> </tr>
-<tr> <td>DIVISION:</td> <td>$div</td> </tr>
-<tr> <td>CITY:</td> <td>$city</td> </tr>
-<tr> <td>ADDRESS:</td> <td>$address</td> </tr>
-<tr> <td>MOBILE NO:</td> <td>$mobileno</td> </tr>
-<tr> <td>PIN:</td> <td>$pincode</td> </tr>
-<tr> <td>STATE:</td> <td>$state</td> </tr>
-<tr> <td>COUNTRY:</td> <td>$country</td> </tr></table>";
+$username=$_REQUEST['usernm'];
+$Password=$_REQUEST['pwd'];
+$Name=$_REQUEST['nm'];
+$Address=$_REQUEST['adrs'];
+$Country=$_REQUEST['country'];
+$Zip_code=$_REQUEST['zcode'];
+$Email=$_REQUEST['email'];
+$Sex=$_REQUEST['sex'];
+$language=$_REQUEST['lang'];
+$about=$_REQUEST['detail'];
+
+echo $username;
+echo "<br><br>";
+echo $Password;
+echo "<br><br>";
+echo $Name;
+echo "<br><br>";
+echo $Address;
+echo "<br><br>";
+echo $Country;
+echo "<br><br>";
+echo $Zip_code;
+echo "<br><br>";
+echo $Email;
+echo "<br><br>";
+echo $Sex;
+echo "<br><br>";
+echo $language;
+echo "<br><br>";
+echo $about;
+echo "<br><br>";
+echo "<table> </table>";
 ?>
