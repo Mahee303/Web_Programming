94f5b13fbe165c9ac5bdff8b92c12ef693b566df
Mahee303 <155056179+Mahee303@users.noreply.github.com>
2024-02-21 12:45:18 +0530

    Add files via upload

---
+++ array/que2.php
@@ -0,0 +1,4 @@
+<?php
+$student_detail=array("name"=>"Mahee","rollno"=>44,"address"=>"Rajkot","department"=>"cs&it","university"=>"Atmiya","class"=>"B2","dob"=>"7-11-2004");
+print_r($student_detail);
+?>
